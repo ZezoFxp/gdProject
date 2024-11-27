f33f4f0d1c0b9f78f520d495c726d9c701c747a9
José Messias <software.devjm@gmail.com>
2024-11-27 15:47:58 -0300

    Segundo commit - novas inclusões no código (novos inputs, e selects manipulados com DOM e validações)

--- main.js
+++ main.js
@@ -3,17 +3,29 @@ const nome2 = document.getElementById('nomeReu2');
 const nome3 = document.getElementById('nomeReu3');
 const nome4 = document.getElementById('nomeReu4');
 const nome5 = document.getElementById('nomeReu5');
+const nome6 = document.getElementById('nomeReu6');
+const nome7 = document.getElementById('nomeReu7');
+const nome8 = document.getElementById('nomeReu8');
+const nome9 = document.getElementById('nomeReu9');
+const nome10 = document.getElementById('nomeReu10');
+
 var jg1 = document.getElementById('jg-reu1'); // Obtém o elemento div
 var jg2 = document.getElementById('jg-reu2'); // Obtém o elemento div
 var jg3 = document.getElementById('jg-reu3'); // Obtém o elemento div
 var jg4 = document.getElementById('jg-reu4'); // Obtém o elemento div
 var jg5 = document.getElementById('jg-reu5'); // Obtém o elemento div
+var jg6 = document.getElementById('jg-reu6'); // Obtém o elemento div
+var jg7 = document.getElementById('jg-reu7'); // Obtém o elemento div
+var jg8 = document.getElementById('jg-reu8'); // Obtém o elemento div
+var jg9 = document.getElementById('jg-reu9'); // Obtém o elemento div
+var jg10 = document.getElementById('jg-reu10'); // Obtém o elemento div
+
 var select = document.getElementById('opt-reus'); // Obtém o <select> com id 'opt-reus'
 var labelReu1 = document.getElementById('sim-ou-nao1');
 
 
-function ocultarTodasAsDivs() {
-    [jg1, jg2, jg3, jg4, jg5].forEach((div) => {
+function ocultarTodasAsJg() {
+    [jg1, jg2, jg3, jg4, jg5, jg6, jg7, jg8, jg9, jg10].forEach((div) => {
         div.style.display = "none";
     });
 }
@@ -21,7 +33,8 @@ function ocultarTodasAsDivs() {
 // Verifica se a opção selecionada é "1"
 select.addEventListener('change', function () {
 
-    ocultarTodasAsDivs();
+    
+    ocultarTodasAsJg();
 
     switch (select.value) {
         case "1":
@@ -54,5 +67,60 @@ select.addEventListener('change', function () {
             jg4.style.display = "block";
             jg5.style.display = "block";
             break;
-    }
+        case "6":
+            labelReu1.textContent = nome.value;
+            jg1.style.display = "block";
+            jg2.style.display = "block";
+            jg3.style.display = "block";
+            jg4.style.display = "block";
+            jg5.style.display = "block";
+            jg6.style.display = "block";
+            break;
+        case "7":
+            labelReu1.textContent = nome.value;
+            jg1.style.display = "block";
+            jg2.style.display = "block";
+            jg3.style.display = "block";
+            jg4.style.display = "block";
+            jg5.style.display = "block";
+            jg6.style.display = "block";
+            jg7.style.display = "block";           
+            break;
+        case "8":
+            labelReu1.textContent = nome.value;
+            jg1.style.display = "block";
+            jg2.style.display = "block";
+            jg3.style.display = "block";
+            jg4.style.display = "block";
+            jg5.style.display = "block";
+            jg6.style.display = "block";
+            jg7.style.display = "block";           
+            jg8.style.display = "block";           
+            break;
+        case "9":
+            labelReu1.textContent = nome.value;
+            jg1.style.display = "block";
+            jg2.style.display = "block";
+            jg3.style.display = "block";
+            jg4.style.display = "block";
+            jg5.style.display = "block";
+            jg6.style.display = "block";
+            jg7.style.display = "block";           
+            jg8.style.display = "block";           
+            jg9.style.display = "block";           
+            break;
+        case "10":
+            labelReu1.textContent = nome.value;
+            jg1.style.display = "block";
+            jg2.style.display = "block";
+            jg3.style.display = "block";
+            jg4.style.display = "block";
+            jg5.style.display = "block";
+            jg6.style.display = "block";
+            jg7.style.display = "block";           
+            jg8.style.display = "block";           
+            jg9.style.display = "block";           
+            jg10.style.display = "block";
+            break;
+        }
 });
---
+++ validaProcessos.js
@@ -0,0 +1,134 @@
+const qtdAcoes = document.getElementById('qtd-acoes');
+// pega a div inteira de quantidade de ações
+
+var numAcao1 = document.getElementById('acoes-tipo1');
+// pega a primeira div contendo os inputs e select do tipo de ação + numero do processo
+
+var numAcao2 = document.getElementById('acoes-tipo2'); 
+// pega a segunda div contendo os inputs e select do tipo de ação + numero do processo
+
+var numAcao3 = document.getElementById('acoes-tipo3');
+// pega a terceira div contendo os inputs e select do tipo de ação + numero do processo
+var numAcao4 = document.getElementById('acoes-tipo4'); // pega a quarta div contendo os inputs e select do tipo de ação + numero do processo
+var numAcao5 = document.getElementById('acoes-tipo5'); // pega a quinta div contendo os inputs e select do tipo de ação + numero do processo
+var numAcao6 = document.getElementById('acoes-tipo6'); // pega a sexta div contendo os inputs e select do tipo de ação + numero do processo
+var numAcao7 = document.getElementById('acoes-tipo7'); // pega a sétima div contendo os inputs e select do tipo de ação + numero do processo
+var numAcao8 = document.getElementById('acoes-tipo8'); // pega a oitava div contendo os inputs e select do tipo de ação + numero do processo
+var numAcao9 = document.getElementById('acoes-tipo9'); // pega a nona div contendo os inputs e select do tipo de ação + numero do processo
+var numAcao10 = document.getElementById('acoes-tipo10'); // pega a décima div contendo os inputs e select do tipo de ação + numero do processo
+
+var tipoAcoes = document.getElementById('tipo-acoes'); // pega TODA a div que escrevemos os numeros e selcionamos o tipo de ação
+var selectAcoes = document.getElementById('acoes-select'); // pega o select da quantidade de ações
+var selectTipo1 = document.getElementById('select-tipo') // pega o select do tipo de cada ação
+var selectTipo2 = document.getElementById('select-tipo') // pega o select do tipo de cada ação
+var selectTipo3 = document.getElementById('select-tipo') // pega o select do tipo de cada ação
+var selectTipo4 = document.getElementById('select-tipo') // pega o select do tipo de cada ação
+var selectTipo5 = document.getElementById('select-tipo') // pega o select do tipo de cada ação
+var selectTipo6 = document.getElementById('select-tipo') // pega o select do tipo de cada ação
+var selectTipo7 = document.getElementById('select-tipo') // pega o select do tipo de cada ação
+var selectTipo8 = document.getElementById('select-tipo') // pega o select do tipo de cada ação
+var selectTipo9 = document.getElementById('select-tipo') // pega o select do tipo de cada ação
+var selectTipo10 = document.getElementById('select-tipo') // pega o select do tipo de cada ação
+
+var contrato1 = document.getElementById('contratos1'); //pega o input do contrato 1
+var cda1 = document.getElementById('cda1'); //pega o input da CDA 1
+
+function ocultarContrato(){                         //oculta os campos de contrato e cda
+    [contrato1, cda1].forEach((contrato) => {
+        contrato.style.display = "none"
+    })
+};
+
+function ocultarNumAcao(){
+    [numAcao1, numAcao2, numAcao3, numAcao5, numAcao6, numAcao7, numAcao8, numAcao9, numAcao10].forEach((acao) => {
+        acao.style.display = "none";
+    });
+};
+
+
+selectAcoes.addEventListener('change', function() {
+    
+
+    ocultarNumAcao();
+
+    switch(selectAcoes.value){
+        case "1":
+            numAcao1.style.display = "block"
+            break;
+        case "2":
+            numAcao1.style.display = "block"
+            numAcao2.style.display = "block"
+            break;
+        case "3":
+            numAcao1.style.display = "block"
+            numAcao2.style.display = "block"
+            numAcao3.style.display = "block"
+            break;
+        case "4":
+            numAcao1.style.display = "block"
+            numAcao2.style.display = "block"
+            numAcao3.style.display = "block"
+            numAcao4.style.display = "block"
+            break;
+        case "5":
+            numAcao1.style.display = "block"
+            numAcao2.style.display = "block"
+            numAcao3.style.display = "block"
+            numAcao4.style.display = "block"
+            numAcao5.style.display = "block"
+            break;
+        case "6":
+            numAcao1.style.display = "block"
+            numAcao2.style.display = "block"
+            numAcao3.style.display = "block"
+            numAcao4.style.display = "block"
+            numAcao5.style.display = "block"
+            numAcao6.style.display = "block"
+            break;
+        case "7":
+            numAcao1.style.display = "block"
+            numAcao2.style.display = "block"
+            numAcao3.style.display = "block"
+            numAcao4.style.display = "block"
+            numAcao5.style.display = "block"
+            numAcao6.style.display = "block"
+            numAcao7.style.display = "block"
+            break;
+        case "8":
+            numAcao1.style.display = "block"
+            numAcao2.style.display = "block"
+            numAcao3.style.display = "block"
+            numAcao4.style.display = "block"
+            numAcao5.style.display = "block"
+            numAcao6.style.display = "block"
+            numAcao7.style.display = "block"
+            numAcao8.style.display = "block"
+            break;
+        case "9":
+            numAcao1.style.display = "block"
+            numAcao2.style.display = "block"
+            numAcao3.style.display = "block"
+            numAcao4.style.display = "block"
+            numAcao5.style.display = "block"
+            numAcao6.style.display = "block"
+            numAcao7.style.display = "block"
+            numAcao8.style.display = "block"
+            numAcao9.style.display = "block"
+            break;
+        case "10":
+            numAcao1.style.display = "block"
+            numAcao2.style.display = "block"
+            numAcao3.style.display = "block"
+            numAcao4.style.display = "block"
+            numAcao5.style.display = "block"
+            numAcao6.style.display = "block"
+            numAcao7.style.display = "block"
+            numAcao8.style.display = "block"
+            numAcao9.style.display = "block"
+            numAcao10.style.display = "block"
+            break;
+            
+            
+        };
+
+});
